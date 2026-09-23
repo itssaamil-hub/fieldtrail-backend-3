9365745bb61f5c8be2bdb49ee07bc1422660bec8
itssaamil-hub <itssaamil@gmail.com>
2026-09-24 01:11:34 +0530

    Enable database-backed dashboard comparisons

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
 import App from "./App.jsx";
 import "./index.css";
+import "./dashboardComparisons.js";
 
 // Silent auto-update: when a new version is deployed, the next launch just
 // picks it up. A field team shouldn't have to think about "updating an app."
